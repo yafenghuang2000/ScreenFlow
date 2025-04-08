20ca07fee047d8a651e3dcbc99b7408a26cc2271
yafenghuang2000 <yafenghuang2000@gmail.com>
2025-04-08 23:27:43 +0800

    feat(router): 添加未找到页面和用户认证逻辑

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,29 @@
 import { lazy } from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom';
+import { getLocalStorage } from '@/utils/StorageValue';
 
 const Laylout = lazy(() => import('@/Layout'));
+const NotFoundPage = lazy(() => import('@/pages/NotFoundPage'));
+
+const isUserAuthenticated = () => {
+  const user = getLocalStorage('user');
+  if (!user) {
+    return redirect('/login');
+  }
+  return null;
+};
 
 const routers = createBrowserRouter([
   {
     path: '/',
     element: <Laylout />,
+    loader: isUserAuthenticated,
+    errorElement: <p>Error</p>,
     children: [],
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
   },
 ]);
 
