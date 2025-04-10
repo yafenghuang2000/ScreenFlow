10735628eac1fe832d3f467d9265dc0125e1498e
huangyafeng <p-huangyafeng@xiaomi.com>
2025-04-10 15:14:59 +0800

    feat: 123

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  '*.js': ['eslint --fix', 'prettier --write'],
+  '*.ts': ['eslint --fix', 'prettier --write'],
+  '*.tsx': ['eslint --fix', 'prettier --write'],
+  '*.scss': ['prettier --write'],
+};
